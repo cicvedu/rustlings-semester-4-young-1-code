9adfb0ae4dd11d43bdb9f0d1876f98485c979fb5
young <2863790348@qq.com>
2024-07-09 22:06:52 +0800

    finish...

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,7 +27,6 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
@@ -36,8 +35,15 @@ extern "Rust" {
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    #[no_mangle]
+    pub extern "Rust" fn my_demo_function(a: u32) -> u32 {
         a
+    }
+
+    // Create an alias for `my_demo_function`
+    #[no_mangle]
+    pub extern "Rust" fn my_demo_function_alias(a: u32) -> u32 {
+        my_demo_function(a)
     }
 }
 
@@ -54,8 +60,8 @@ mod tests {
         // SAFETY: We know those functions are aliases of a safe
         // Rust function.
         unsafe {
-            my_demo_function(123);
-            my_demo_function_alias(456);
+            assert_eq!(my_demo_function(123), 123);
+            assert_eq!(my_demo_function_alias(456), 456);
         }
     }
 }
